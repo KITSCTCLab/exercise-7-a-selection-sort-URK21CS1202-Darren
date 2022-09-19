83a7081d7e0bae80d2a54f886eb8537b63ce60f6
URK21CS1202-Darren <109952116+URK21CS1202-Darren@users.noreply.github.com>
2022-09-19 14:10:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,28 @@
 from typing import List
-def selectionSort(array, size) -> List[int]:                      
-  for step in range(size):                               
-    min_idx = step
-    for i in range(step + 1, size):                               
-      if array[i] < array[min_idx]:
-        min_idx = i
-    (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+def selectionSort(array, size) -> List[int]:
+  """
+  Function implementing selection sort algorithm to return the passed array sorted in the ascending order
+  """
+  for index in range(size):
+    min_index = index
+    for ind in range(index + 1, size):
+      if array[ind] < array[min_index]:
+        min_index = ind
+    array[min_index], array[index] = array[index], array[min_index]
   return array
-                                                                                                             
+
+# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
-  if item.isnumeric():                            
+  if item.isnumeric():
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
-    data.append(int(item))                                  
-print(selectionSort(data, len(data)))                      
+    data.append(int(item))
+print(selectionSort(data, len(data)))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privac
